228f56c5ec679ff3b6e2e6a3a1fff179b44360e3
Rena Sakata <rena.sakata.cn@gmail.com>
2023-11-27 23:13:47 -0800

    add function for outputting transcript to file

--- src/SpeechToTextComponent.js
+++ src/SpeechToTextComponent.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as sdk from 'microsoft-cognitiveservices-speech-sdk';
+import { saveAs } from 'file-saver';
 
 const SPEECH_KEY = process.env.REACT_APP_SPEECH_KEY;
 const SPEECH_REGION = process.env.REACT_APP_SPEECH_REGION;
 
-export function SpeechToTextComponent() {
+const SpeechToTextComponent = () => {
   const [isListening, setIsListening] = useState(false);
   const speechConfig = useRef(null);
   const audioConfig = useRef(null);
   const recognizer = useRef(null);
-
-  const [myTranscript, setMyTranscript] = useState('');
-  const [recognizingTranscript, setRecTranscript] = useState('');
+  const [recognizingTranscript, setRecognizingTranscript] = useState('');
+  const [recognizedTranscript, setRecognizedTranscript] = useState('');
 
   useEffect(() => {
     speechConfig.current = sdk.SpeechConfig.fromSubscription(
@@ -26,36 +26,23 @@ export function SpeechToTextComponent() {
       audioConfig.current
     );
 
-    const processRecognizedTranscript = (event) => {
-      const result = event.result;
-      console.log('Recognition result:', result);
-
+    recognizer.current.recognized = (s, e) => {
+      const result = e.result;
       if (result.reason === sdk.ResultReason.RecognizedSpeech) {
-        const transcript = result.text;
-        console.log('Transcript: -->', transcript);
-        // Call a function to process the transcript as needed
-
-        setMyTranscript(transcript);
+        setRecognizedTranscript(
+          (prevTranscript) => prevTranscript + result.text + '\n'
+        );
       }
     };
 
-    const processRecognizingTranscript = (event) => {
-      const result = event.result;
-      console.log('Recognition result:', result);
+    recognizer.current.recognizing = (s, e) => {
+      const result = e.result;
       if (result.reason === sdk.ResultReason.RecognizingSpeech) {
-        const transcript = result.text;
-        console.log('Transcript: -->', transcript);
-        // Call a function to process the transcript as needed
-
-        setRecTranscript(transcript);
+        setRecognizingTranscript(result.text);
       }
     };
-
-    recognizer.current.recognized = (s, e) => processRecognizedTranscript(e);
-    recognizer.current.recognizing = (s, e) => processRecognizingTranscript(e);
 
     recognizer.current.startContinuousRecognitionAsync(() => {
-      console.log('Speech recognition started.');
       setIsListening(true);
     });
 
@@ -69,36 +56,39 @@ export function SpeechToTextComponent() {
   const pauseListening = () => {
     setIsListening(false);
     recognizer.current.stopContinuousRecognitionAsync();
-    console.log('Paused listening.');
   };
 
   const resumeListening = () => {
     if (!isListening) {
       setIsListening(true);
-      recognizer.current.startContinuousRecognitionAsync(() => {
-        console.log('Resumed listening...');
-      });
+      recognizer.current.startContinuousRecognitionAsync();
     }
   };
 
   const stopListening = () => {
     setIsListening(false);
     recognizer.current.stopContinuousRecognitionAsync(() => {
-      console.log('Speech recognition stopped.');
+      saveTranscript(recognizedTranscript);
     });
+  };
+
+  const saveTranscript = (transcript) => {
+    const blob = new Blob([transcript], { type: 'text/plain;charset=utf-8' });
+    saveAs(blob, 'transcript.txt');
   };
 
   return (
     <div>
-      <button onClick={pauseListening}>Pause Listening</button>
       <button onClick={resumeListening}>Resume Listening</button>
+      <button onClick={pauseListening}>Pause Listening</button>
       <button onClick={stopListening}>Stop Listening</button>
 
       <div>
-        <div>Recognizing Transcript : {recognizingTranscript}</div>
-
-        <div>RecognizedTranscript : {myTranscript}</div>
+        <div>Recognizing Transcript: {recognizingTranscript}</div>
+        <div>Recognized Transcript: {recognizedTranscript}</div>
       </div>
     </div>
   );
-}
+};
+
+export default SpeechToTextComponent;
